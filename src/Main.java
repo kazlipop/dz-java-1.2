8fbd807335983d64685c609d5dc4bd4238d5d897
kazlipop <88086013+kazlipop@users.noreply.github.com>
2022-02-17 16:48:13 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int price = 2_000_000_000;
         int count = 500_000_000;
-        int total = price - count;
+        int total = price + count;
         System.out.println(total);
     }
 }
